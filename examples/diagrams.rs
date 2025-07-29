f3d5f190f43a156afb7365ba383410c2da48f989
Peter Arato <it.arato@gmail.com>
2025-07-29 08:10:24 -0400

    Diagrams

---
+++ examples/diagrams.rs
@@ -0,0 +1,116 @@
+use crossterm::event::{Event, MouseButton, MouseEventKind};
+use log::{error, info};
+use terge::Terge;
+
+type I32Point = (i32, i32);
+const BLOCK_CHAR: &'static str = "█";
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum DrawMode {
+    Nothing,
+    Line,
+    Rect,
+}
+
+struct DrawAction {
+    mode: DrawMode,
+    start: I32Point,
+}
+
+struct App {
+    draw_mode_details: Option<DrawAction>,
+    draw_mode_indent: DrawMode,
+    current_mouse_pos: I32Point,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            draw_mode_details: None,
+            draw_mode_indent: DrawMode::Rect,
+            current_mouse_pos: (-1, -1),
+        }
+    }
+
+    fn start_draw_mode(&mut self, start: I32Point) {
+        if self.draw_mode_details.is_some() {
+            error!("Starting draw mode when there is already one.");
+            return;
+        }
+
+        if self.draw_mode_indent == DrawMode::Nothing {
+            info!("Rejecting draw mode - no intent.");
+            return;
+        }
+
+        self.draw_mode_details = Some(DrawAction {
+            mode: self.draw_mode_indent,
+            start,
+        });
+    }
+
+    fn end_draw_mode(&mut self) {
+        self.draw_mode_details = None;
+    }
+}
+
+impl terge::App for App {
+    fn draw(&self, gfx: &mut terge::Gfx) {
+        gfx.clear_screen();
+
+        if let Some(draw_action) = &self.draw_mode_details {
+            let start_x = draw_action.start.0.min(self.current_mouse_pos.0);
+            let end_x = draw_action.start.0.max(self.current_mouse_pos.0);
+            let start_y = draw_action.start.1.min(self.current_mouse_pos.1);
+            let end_y = draw_action.start.1.max(self.current_mouse_pos.1);
+
+            for y in start_y..=end_y {
+                gfx.draw_text(BLOCK_CHAR, start_x as usize, y as usize);
+                gfx.draw_text(BLOCK_CHAR, end_x as usize, y as usize);
+            }
+            gfx.draw_text(
+                &BLOCK_CHAR.repeat((end_x - start_x) as usize),
+                start_x as usize,
+                start_y as usize,
+            );
+            gfx.draw_text(
+                &BLOCK_CHAR.repeat((end_x - start_x) as usize),
+                start_x as usize,
+                end_y as usize,
+            );
+        }
+    }
+
+    fn reset(&mut self, gfx: &mut terge::Gfx) {}
+
+    fn update(&mut self, events: &terge::EventGroup, gfx: &mut terge::Gfx) -> bool {
+        for e in &events.events {
+            match e {
+                Event::Mouse(mouse_event) => {
+                    if mouse_event.kind == MouseEventKind::Down(MouseButton::Left) {
+                        self.start_draw_mode((mouse_event.column as i32, mouse_event.row as i32));
+                    }
+                    if mouse_event.kind == MouseEventKind::Up(MouseButton::Left) {
+                        self.end_draw_mode();
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if let Some(last_mouse_pos) = events.last_mouse_pos() {
+            self.current_mouse_pos.0 = last_mouse_pos.0 as i32;
+            self.current_mouse_pos.1 = last_mouse_pos.1 as i32;
+        }
+
+        true
+    }
+}
+
+fn main() {
+    pretty_env_logger::init();
+
+    let mut engine = Terge::new(Box::new(App::new()));
+    engine.set_target_fps(60);
+    engine.run();
+}
